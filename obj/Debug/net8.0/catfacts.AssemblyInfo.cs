6a4aa26096072bdeef5bb113721fcff7e3031c76
Steve C <escodsm@gmail.com>
2025-07-10 12:16:36 -0400

    gitignore bin, obj, readme, log file

--- obj/Debug/net8.0/catfacts.AssemblyInfo.cs
+++ obj/Debug/net8.0/catfacts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatFacts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ead560e4b0f11249a09af008f3b2f5b8da30d58")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatFacts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatFacts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
