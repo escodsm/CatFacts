046a3d7948e73e4ea96618c0b5d8d1b41ca77e0a
Steve C <escodsm@gmail.com>
2025-07-28 09:55:00 -0400

    applied fixes to code, working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,32 @@
+using CatFacts.Models;
+using CatFacts.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CatFacts.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly CatFactService _catFactService;
+
+        public HomeController(CatFactService catFactService)
         {
-            return Content("It works!");
+            _catFactService = catFactService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var facts = await _catFactService.FetchAndProcessCatFacts();
+                return View(facts);
+            }
+            catch
+            {
+                ViewBag.ErrorMessage = "Failed to load cat facts.";
+                return View(new List<CatFact>()); // show error in view
+            }
         }
     }
 }
--- Models/CatFact.cs
+++ Models/CatFact.cs
@@ -1,10 +1,20 @@
+using System;
+using System.Text.Json.Serialization;
+
 namespace CatFacts.Models
 {
     public class CatFact
     {
+        [JsonPropertyName("fact")]
         public string Text { get; set; }
+
+        [JsonPropertyName("used")]
         public bool Used { get; set; }
+
+        [JsonPropertyName("createdAt")]
         public DateTime CreatedAt { get; set; }
+
+        // Not from API, used internally
         public string LogStatus { get; set; }
     }
 }
